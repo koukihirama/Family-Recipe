db29923837ea0bb5bee0ccc686c528ae6ea9f3ea
平間洸揮 <hiramakouki@MacBook-Pro.local>
2025-07-07 14:59:01 +0900

    ⭐ボタンの修正

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,15 +2,15 @@ module RecipesHelper
     def difficulty_label(level)
     case level
     when 1
-      "⭐️ とても簡単"
+      "★ とても簡単"
     when 2
-      "⭐️⭐️ 簡単"
+      "★★ 簡単"
     when 3
-      "⭐️⭐️⭐️ 普通"
+      "★★★ 普通"
     when 4
-      "⭐️⭐️⭐️⭐️ 少しむずかしい"
+      "★★★★ 少しむずかしい"
     when 5
-      "⭐️⭐️⭐️⭐️⭐️ むずかしい"
+      "★★★★★ むずかしい"
     else
       ""
     end
